194d4b8ee09529f39744d9d348e44d41fc3e73a8
Marcel <sansuer.marcel@gmail.com>
2017-07-03 22:21:37 +0200

    debug-meshdata[32] error Refactoring MapGenerator

--- Assets/Scripts/PerlinNoise/MapGenerator.cs
+++ Assets/Scripts/PerlinNoise/MapGenerator.cs
@@ -26,50 +26,70 @@ public class MapGenerator : MonoBehaviour {
 	public Regoins[] regions;
 	public bool autoUpdate;
 
-	public void GenerateMap() {
-		OnValuedate ();
+	public void GenerateMap()
+    {
+        OnValuedate();
 
-		float[,] noiseMap = Noise.GenerateNoiseMap (mapChunkSize
-, mapChunkSize, noiseScale, octaves, persistence,lacunarity, seed, myOffset);
+        float[,] noiseMap = Noise.GenerateNoiseMap(mapChunkSize, mapChunkSize, noiseScale, octaves, persistence, lacunarity, seed, myOffset);
 
-		//Color the 2d map 
-		Color[] color = new Color[mapChunkSize * mapChunkSize
-];
-		for (int y = 0; y < mapChunkSize; ++y) {
-			for (int x = 0; x < mapChunkSize
-	; ++x) {
-				float currentheight = noiseMap [x, y];
+        //Color the 2d map 
+        Color[] color = new Color[mapChunkSize * mapChunkSize];
+        colorTheMap(noiseMap, color);
+		//get output
+        getDisplay(noiseMap, color);
+    }
 
-				for(int i = 0; i < regions.Length; i++){
-					if (currentheight <= regions [i].height) { 
-						color [y * mapChunkSize
-				 + x] = regions [i].color;
-						break;
-					}
-				}
-			}
-		}
+// show the result to the screen
+    private void getDisplay(float[,] noiseMap, Color[] color)
+    {
+        if (FindObjectOfType<MapDisplay>())
+        {
+            MapDisplay display = FindObjectOfType<MapDisplay>();
 
-		if (FindObjectOfType<MapDisplay> ()) {
-			MapDisplay display = FindObjectOfType<MapDisplay> ();
+            if (mode == DrawMode.Noise)
+            {
+                display.drawTexture2D(GenerateTexture.TextureFromNoiseMap(noiseMap));
+            }
+            else if (mode == DrawMode.Color)
+            {
+                display.drawTexture2D(GenerateTexture.textureFromColorMap(mapChunkSize
+        , mapChunkSize, color));
+            }
+            else if (mode == DrawMode.Mesh)
+            {
+                display.drawMesh(MashGenerator.GenerateMash(noiseMap, heightMultiplier, meshHeightCurve, levelOfDetail), GenerateTexture.textureFromColorMap(mapChunkSize
+        , mapChunkSize, color));
+            }
+        }
+        else
+        {
+            print("Not found");
+        }
+    }
 
-			if (mode == DrawMode.Noise) {
-				display.drawTexture2D (GenerateTexture.TextureFromNoiseMap (noiseMap));
-			} else if (mode == DrawMode.Color) {
-				display.drawTexture2D (GenerateTexture.textureFromColorMap (mapChunkSize
-		, mapChunkSize, color));
-			} else if (mode == DrawMode.Mesh) {
-				display.drawMesh(MashGenerator.GenerateMash (noiseMap, heightMultiplier, meshHeightCurve, levelOfDetail), GenerateTexture.textureFromColorMap (mapChunkSize
-		, mapChunkSize, color));
-			}
+    //get color of heightvalues
+    private void colorTheMap(float[,] noiseMap, Color[] color)
+    {
+        for (int y = 0; y < mapChunkSize; ++y)
+        {
+            for (int x = 0; x < mapChunkSize; ++x)
+            {
+                float currentheight = noiseMap[x, y];
 
+                for (int i = 0; i < regions.Length; i++)
+                {
+                    if (currentheight <= regions[i].height)
+                    {
+                        color[y * mapChunkSize
+                 + x] = regions[i].color;
+                        break;
+                    }
+                }
+            }
+        }
+    }
 
-		} else {
-			print ("Not found");
-		}
-	}
-
-	[System.Serializable]
+    [System.Serializable]
 	public struct Regoins{
 		public string name;
 		public float height;
--- Assets/Scripts/PerlinNoise/MashGenerator.cs
+++ Assets/Scripts/PerlinNoise/MashGenerator.cs
@@ -7,20 +7,31 @@ public static class MashGenerator {
 		int width = heightMap.GetLength (0);
 		int height = heightMap.GetLength (1);
 
-		MeshData meshData = new MeshData (height, width);
+		
 		int vertexIndex = 0;
 
+		int simplifiMesh;
+
+		if(levelOfDetail == 0){
+			simplifiMesh = 1;
+		}else{
+			simplifiMesh = levelOfDetail * 2;
+		}
+
+		int vertecPerLine = (width - 1) / simplifiMesh + 1;
+		MeshData meshData = new MeshData (vertecPerLine, vertecPerLine);
+		
 		float topLeftX = (width - 1) / 2f;
 		float topLeftZ = (height - 1) / 2f;
 
-		for (int y = 0; y < height; ++y) {
-			for (int x = 0; x < width; ++x) {
+		for (int y = 0; y < height; y += simplifiMesh) {
+			for (int x = 0; x < width; x += simplifiMesh) {
 
 				meshData.vertices [vertexIndex] = new Vector3 (topLeftX + x, meshHeightCurve.Evaluate(heightMap [x, y]) * heightMultiplier,topLeftZ - y);
 				meshData.uvs [vertexIndex] = new Vector2(x / (float)width, y / (float)height);
 				if (x < width - 1 && y < height - 1) {
-					meshData.addTraingle (vertexIndex, vertexIndex + width + 1, vertexIndex + width);
-					meshData.addTraingle (vertexIndex + width + 1, vertexIndex, vertexIndex + 1);
+					meshData.addTraingle (vertexIndex, vertexIndex + vertecPerLine + 1, vertexIndex + vertecPerLine);
+					meshData.addTraingle (vertexIndex + vertecPerLine + 1, vertexIndex, vertexIndex + 1);
 				}
 				vertexIndex++;
 			}
